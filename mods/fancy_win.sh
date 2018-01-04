d013c5a52a2d573a66b414c9b8d1521b7ab6be43
Phoenix1747 <apertured@public-files.de>
2018-01-04 20:42:43 +0100

    fixed mod execution

---
+++ mods/fancy_win.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This is a really small demo mod for ASCIIrinth.
+# It makes the win message much more fancy!
+
+TEXT_won="
+\033[1;34m                                  __
+\033[1;34m     __ __            _ _ _ _    |  |
+\033[1;35m    |  |  |___ _ _   | | | |_|___|  |
+\033[1;31m    |_   _| . | | |  | | | | |   |__|
+\033[1;33m      |_| |___|___|  |_____|_|_|_|__|
+\033[0m
+  "
